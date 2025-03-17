4ca6034b85b01c9c7fe9bebf1b659165c487a208
Paul Bae <baepaul@amazon.com>
2025-03-17 10:10:16 -0400

    redoing 11

--- leetcode/11.py
+++ leetcode/11.py
@@ -3,19 +3,18 @@ from typing import List
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         max_area = 0
-        l, r = 0, len(height) - 1
+        left, right = 0, len(height) - 1
 
-        while l < r:
-            max_area = max(max_area, (r - l) * min(height[l], height[r]))
-            if height[l] < height[r]:
-                l += 1
+        while left < right:
+            max_area = max(max_area, min(height[left], height[right]) * (right - left))
+            if height[left] < height[right]:
+                left += 1
             else:
-                r -= 1
+                right -= 1
 
         return max_area
-        
 
 sol = Solution()
-print(sol.maxArea([1,8,6,2,5,4,8,3,7]))
-print(sol.maxArea([1,1]))
-
+print(sol.maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]))
+print(sol.maxArea([1, 2, 4, 3]))
+print(sol.maxArea([1, 1]))
