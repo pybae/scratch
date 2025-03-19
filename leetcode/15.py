09526667571f84f97982a6c24911a6785d791b05
Paul Bae <pbae@utexas.edu>
2025-03-19 09:26:19 -0400

    15

--- leetcode/15.py
+++ leetcode/15.py
@@ -2,25 +2,21 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        ans = set()
+        result = set()
         nums.sort()
 
-        for i in range(len(nums)):
+        for i in range(len(nums) - 1):
             j, k = i + 1, len(nums) - 1
             while j < k:
                 if nums[i] + nums[j] + nums[k] == 0:
-                    ans.add((nums[i], nums[j], nums[k]))
+                    result.add((nums[i], nums[j], nums[k]))
                     j += 1
                 elif nums[i] + nums[j] + nums[k] < 0:
                     j += 1
                 else:
                     k -= 1
 
-        return [[t[0], t[1], t[2]] for t in ans]
-
+        return [[t[0], t[1], t[2]] for t in result]
 
 sol = Solution()
-# -4, -1, -1, 0, 1, 2
-print(sol.threeSum([-1,0,1,2,-1,-4]))
-print(sol.threeSum([0,1,1]))
-print(sol.threeSum([0,0,0]))
+print(sol.threeSum([-1, 0, 1, 2, -1, -4]))
