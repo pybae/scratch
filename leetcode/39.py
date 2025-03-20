3ee33c998cc869a73a5d2c02396d7f952bbe4349
Paul Bae <pbae@utexas.edu>
2025-03-20 16:13:35 -0400

    re-learning dp.

--- leetcode/39.py
+++ leetcode/39.py
@@ -2,26 +2,23 @@ from typing import List
 
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        dp = [None] * (target + 1)
         candidates.sort()
-        return self.combinationSumRecursive(candidates, target)
-
-    def combinationSumRecursive(self, candidates, target):
-        result = []
-        for candidate in candidates:
-            if candidate == target:
-                result += [[target]]
-            elif candidate < target:
-                subresults = self.combinationSumRecursive(candidates, target - candidate)
-                for subresult in subresults:
-                    subresult.append(candidate)
-                    result.append(subresult)
-            else:
-                break
-
-        return result
+        for i in range(target + 1):
+            dp[i] = []
+            for num in candidates:
+                if num > i:
+                    break
+                elif num == i:
+                    dp[i].append([num])
+                elif dp[i - num]:
+                    dp[i].extend(c + [num] for c in dp[i - num])
+            dp[i] = [list(x) for x in list(set([tuple(sorted(c)) for c in dp[i]]))]
+        return dp[target]
 
 
 sol = Solution()
 print(sol.combinationSum([2, 3, 6, 7], 7))
-# print(sol.combinationSum([2, 3, 5], 8))
-# print(sol.combinationSum([2], 1))
+print(sol.combinationSum([8, 7, 4, 3], 11))
+print(sol.combinationSum([2, 3, 5], 8))
+print(sol.combinationSum([2], 1))
