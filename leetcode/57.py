2a60489b7009b0e67fe38188bfbc348665963c90
Paul Bae <pbae@utexas.edu>
2025-03-21 12:41:07 -0400

    getting better

--- leetcode/57.py
+++ leetcode/57.py
@@ -1,26 +1,45 @@
 from typing import List
 
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        result = []
-        added = False
-        if len(intervals) == 0:
-            return [newInterval]
+    def insert(self, intervals: List[List[int]], new_interval: List[int]) -> List[List[int]]:
+        if not intervals:
+            return [new_interval]
 
-        for interval in intervals:
-            if newInterval[0] <= interval[0] <= newInterval[1] or interval[0] <= newInterval[0] <= interval[1]:
-                newInterval[0] = min(interval[0], newInterval[0])
-                newInterval[1] = max(interval[1], newInterval[1])
-            elif newInterval[1] <= interval[0] and not added:
-                added = True
-                result.append(newInterval)
-                result.append(interval)
+        i = j = self.find_overlapping_interval(intervals, new_interval)
+
+        while j < len(intervals) and self.overlaps(intervals[j], new_interval):
+            new_interval = self.merge(intervals[j], new_interval)
+            j += 1
+
+        if i == j:
+            intervals.insert(i + 1, new_interval)
+        else:
+            intervals[i:j] = [new_interval]
+        return intervals
+
+    def find_overlapping_interval(self, intervals: List[List[int]], new_interval: List[int]) -> int:
+        l, r = 0, len(intervals) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if intervals[mid][0] <= new_interval[0] <= intervals[mid][1]:
+                return mid
+            elif intervals[mid][0] > new_interval[0]:
+                r = mid - 1
             else:
-                result.append(interval)
+                l = mid + 1
+        
+        return l if new_interval[0] < intervals[mid][0] else r
 
-        if not added:
-            result.append(newInterval)
-        return result
+    def overlaps(self, a: List[int], b: List[int]) -> bool:
+        return max(a[0], b[0]) <= min(a[1], b[1])
+
+    def merge(self, a: List[int], b: List[int]) -> List[int]:
+        return [min(a[0], b[0]), max(a[1], b[1])]
 
 sol = Solution()
-print(sol.insert([[2,5],[6,7],[8,9]], [0, 1]))
+# print(sol.insert([[1,3],[6,9]], [4,5]))
+# print(sol.insert([[1,3],[6,9]], [2,5]))
+# print(sol.insert([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8]))
+# print(sol.insert([], [5, 7]))
+print(sol.insert([[2, 3], [5, 7]], [0, 6]))
+print(sol.insert([[1, 5]], [0, 0]))
